102ac0d1e15da56692e3ff24f19c0b29803e1b26
David Warwick <bgmsd@live.com>
2021-09-30 16:04:39 -0700

    Added Database, Automapper, UnitOfWork, and DTO's.

---
+++ HotelListing/Model/HotelDTO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelListing.Model
+{
+    public class CreateHotelDTO
+    {
+        [Required]
+        [StringLength(maximumLength: 50, ErrorMessage = "Hotel name cannot be longer than 50 characters.")]
+        public string Name { get; set; }
+        [StringLength(maximumLength: 50, ErrorMessage = "Hotel address cannot be longer than 150 characters.")]
+        [Required]
+        public string Address { get; set; }
+        [Range(1,5)]
+        [Required]
+        public double Rating { get; set; }
+
+        [Required]
+        public int CountryId { get; set; }        
+    }
+
+    public class HotelDTO : CreateCountryDTO
+    {
+        public int Id { get; set; }
+        public CreateCountryDTO Country { get; set; }
+        
+    }
+}
